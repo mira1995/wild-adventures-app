01356cd95a3e0f615e64a7064f5e102eb99c2a33
Luwin95 <b.aubin95@live.fr>
2018-10-07 11:33:03 +0200

    Initial commit : import of spring initializer wauser project

---
+++ wauser/src/main/java/com/wa/msm/user/WaUserApplication.java
@@ -0,0 +1,12 @@
+package com.wa.msm.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaUserApplication.class, args);
+	}
+}
