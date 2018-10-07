2b4e4a29e5752fc6cef319fbbd9811999036bc31
Luwin95 <b.aubin95@live.fr>
2018-10-07 19:22:54 +0200

    Import of microservice image

---
+++ waimage/src/main/java/com/wa/msm/image/WaImageApplication.java
@@ -0,0 +1,12 @@
+package com.wa.msm.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaImageApplication.class, args);
+	}
+}
