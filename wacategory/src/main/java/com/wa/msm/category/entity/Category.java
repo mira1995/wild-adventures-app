3344a8c4143c0b08658504a0faa1660c7bc5b4bc
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-08 15:27:25 +0200

    Update Category entity and add Postman collection

--- wacategory/src/main/java/com/wa/msm/category/entity/Category.java
+++ wacategory/src/main/java/com/wa/msm/category/entity/Category.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
 }
