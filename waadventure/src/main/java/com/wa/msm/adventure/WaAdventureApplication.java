d64c150a698adf41e351f2721a86ad08354fa918
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-08 15:55:08 +0200

    Create MS Adventure project

---
+++ waadventure/src/main/java/com/wa/msm/adventure/WaAdventureApplication.java
@@ -0,0 +1,12 @@
+package com.wa.msm.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaAdventureApplication.class, args);
+	}
+}
