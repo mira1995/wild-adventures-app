94a0c58eeda8464e6a75a4e53a51396547808c24
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-11 13:24:39 +0200

    Update MS Comment : add MSAdventureProxy

--- wacomment/src/main/java/com/wa/msm/comment/WaCommentApplication.java
+++ wacomment/src/main/java/com/wa/msm/comment/WaCommentApplication.java
@@ -2,8 +2,10 @@ package com.wa.msm.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.wa.msm.comment")
 public class WaCommentApplication {
 
 	public static void main(String[] args) {
--- wacomment/src/main/java/com/wa/msm/comment/web/controller/CommentController.java
+++ wacomment/src/main/java/com/wa/msm/comment/web/controller/CommentController.java
@@ -1,7 +1,10 @@
 package com.wa.msm.comment.web.controller;
 
+import com.wa.msm.comment.bean.AdventureBean;
 import com.wa.msm.comment.entity.Comment;
+import com.wa.msm.comment.proxy.MSAdventureProxy;
 import com.wa.msm.comment.repository.CommentRepository;
+import com.wa.msm.comment.web.exception.AdventureNotFoundException;
 import com.wa.msm.comment.web.exception.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +19,14 @@ public class CommentController {
     @Autowired
     CommentRepository commentRepository;
 
+    @Autowired
+    MSAdventureProxy msAdventureProxy;
+
     @GetMapping(value = "/comments/{adventureId}")
     public List<Comment> commentList(@PathVariable Long adventureId) {
+        // Vérifier que l'aventure existe
+        adventureNotFound(adventureId);
+
         List<Comment> comments = new ArrayList<>(0);
         commentRepository.findByAdventureId(adventureId).iterator().forEachRemaining(comments::add);
         if (comments.isEmpty()) throw new CommentNotFoundException("Il n'existe aucun commentaires pour cette aventure à l'id " + adventureId + ".");
@@ -26,16 +35,24 @@ public class CommentController {
 
     @PostMapping(value = "/comment")
     public Comment addComment(@RequestBody Comment comment) {
-        // TODO : Vérifier que l'aventure existe
+        // Vérifier que l'aventure existe
+        adventureNotFound(comment.getAdventureId());
+
         // TODO : Vérifier que l'utilisateur existe
+
         return commentRepository.save(comment);
     }
 
     @PatchMapping(value = "/comment")
     public Comment updateComment(@RequestBody Comment comment) {
-        // TODO : Vérifier que l'aventure existe
-        // TODO : Vérifier que l'utilisateur existe
         if (comment == null) throw new CommentNotFoundException("Le commentaire envoyé n'existe pas.");
+        else {
+            // Vérifier que l'aventure existe
+            adventureNotFound(comment.getAdventureId());
+            comment.getComments().forEach(item -> adventureNotFound(item.getAdventureId()));
+
+            // TODO : Vérifier que l'utilisateur existe
+        }
         return commentRepository.save(comment);
     }
 
@@ -45,5 +62,10 @@ public class CommentController {
         if (!commentToDelete.isPresent()) throw new CommentNotFoundException("Le commentaire correspondant à l'id " + id + " n'existe pas.");
         else commentRepository.deleteById(commentToDelete.get().getId());
         return "Le commentaire pour id " + id + " a bien été supprimé.";
+    }
+
+    private void adventureNotFound(Long adventureId) {
+        Optional<AdventureBean> adventure = msAdventureProxy.getAdventure(adventureId);
+        if (!adventure.isPresent()) throw new AdventureNotFoundException("Il n'existe aucune aventure pour id " + adventureId + ".");
     }
 }
