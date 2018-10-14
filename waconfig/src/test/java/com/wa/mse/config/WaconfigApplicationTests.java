4cc23ef6a5e2e32e19baac7f03a5d657ffec5a7a
Luwin95 <b.aubin95@live.fr>
2018-10-14 21:02:33 +0200

    Adding external config server

---
+++ waconfig/src/test/java/com/wa/mse/config/WaconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wa.mse.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WaconfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
