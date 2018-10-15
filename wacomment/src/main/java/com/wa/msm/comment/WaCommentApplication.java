c57b617197401b07d4ca3e8660038e51ca6e3689
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-15 15:44:28 +0200

    Eureka server and configuration in clients done

--- wacomment/src/main/java/com/wa/msm/comment/WaCommentApplication.java
+++ wacomment/src/main/java/com/wa/msm/comment/WaCommentApplication.java
@@ -2,10 +2,12 @@ package com.wa.msm.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.wa.msm.comment")
+@EnableDiscoveryClient
 public class WaCommentApplication {
 
 	public static void main(String[] args) {
--- waconfig/src/main/java/com/wa/mse/config/ConfigServerApplication.java
+++ waconfig/src/main/java/com/wa/mse/config/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class WaconfigApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WaconfigApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
