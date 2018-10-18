e83ad41e7728e062caba8f9e456c4e7565b612d5
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-18 16:25:35 +0200

    Use of Eureka client instead of Eureka server as a Maven dependency

--- waorder/src/main/java/com/wa/msm/order/WaOrderApplication.java
+++ waorder/src/main/java/com/wa/msm/order/WaOrderApplication.java
@@ -2,12 +2,12 @@ package com.wa.msm.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.wa.msm.order")
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class WaOrderApplication {
 
 	public static void main(String[] args) {
