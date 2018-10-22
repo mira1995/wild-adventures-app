4bba22abfe70bae5d4bc639177c0728437bb1379
Nicolas Bouème <nicolasboueme@gmail.com>
2018-10-22 15:19:29 +0200

    PoC without JWT done

---
+++ client-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Categories from './components/Categories';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Categories />
+      </div>
+    );
+  }
+}
+
+export default App;
