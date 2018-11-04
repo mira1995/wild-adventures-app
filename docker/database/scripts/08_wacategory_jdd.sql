c6c3de78babe72bbfec9ff81eaaedaa810c2db93
Luwin95 <b.aubin95@live.fr>
2018-11-04 16:35:21 +0100

    Added data samples for adventure/sessions and category

---
+++ docker/database/scripts/08_wacategory_jdd.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO wacategory.category (title, description) VALUES 
+	('Nautique','Retrouvez toutes nos aventures nautiques et passez des weekends et vacances sportives dans des cadres maritimes uniques !'),
+	('Montagne',E'Skis, surf, randonnées, planifiez des vacances d\'aventuriers inoubliables dans les plus beau cadres montagneux de la planète.'),
+	('Acrobranche','Envie de prendre de la hauteur? Découvrez nos aventures acrobranches !'),
+	('Trek','Découvrez les plus incroyables sentiers de randonnées du monde réunis dans cette catégorie !'),
+	('Désert',E'Envie d\'évasion? Prenez part à ces aventures extraordinaire à travers les plus beaux déserts qui soient !'),
+	('Thrill travel','Offrez vous des vacances frissons dans les endroits les plus effrayants du monde ! Un concept unique wild adventures'),
+	('Escape game','Consultez nos partenaires escape game à travers le monde.');
